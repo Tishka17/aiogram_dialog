6ae833aebf4e2e908a38f96e1f367add0c5e0348
Tishka17 <tishka17@mail.ru>
2021-02-02 01:13:37 +0300

    Case text widget

--- new/dialog/widgets/text/multi.py
+++ new/dialog/widgets/text/multi.py
@@ -17,3 +17,27 @@ class Multi(Text):
             for t in self.texts
         ]
         return self.sep.join(filter(None, texts))
+
+
+Selector = Callable[[Dict, "Case", DialogManager], bool]
+
+
+def new_case_field(fieldname: str) -> Selector:
+    def case_field(data: Dict, widget: "Case", manager: DialogManager) -> bool:
+        return data.get(fieldname)
+
+    return case_field
+
+
+class Case(Text):
+    def __init__(self, texts: Dict[Any, Text], selector: Union[str, Selector], when: WhenCondition = None):
+        super().__init__(when)
+        self.texts = texts
+        if isinstance(selector, str):
+            self.selector = new_case_field(selector)
+        else:
+            self.selector = selector
+
+    async def _render_text(self, data, manager: DialogManager) -> str:
+        selection = self.selector(data, self, manager)
+        return await self.texts[selection].render_text(data, manager)
