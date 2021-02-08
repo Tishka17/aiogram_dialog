ee79bcb88fdc828e9ded557693e438894ca8f381
Lamroy95 <50185460+Lamroy95@users.noreply.github.com>
2021-02-08 04:51:06 +0300

    Create docs quickstart summary.py

---
+++ docs/examples/quickstart/summary.py
@@ -0,0 +1,35 @@
+from aiogram import Bot, Dispatcher, executor
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import StatesGroup, State
+from aiogram.types import Message
+
+from aiogram_dialog import Window, Dialog, DialogRegistry, DialogManager
+from aiogram_dialog.widgets.kbd import Button
+from aiogram_dialog.widgets.text import Const
+
+storage = MemoryStorage()
+bot = Bot(token='BOT TOKEN HERE')
+dp = Dispatcher(bot, storage=storage)
+registry = DialogRegistry(dp)
+
+
+class MySG(StatesGroup):
+    main = State()
+
+
+main_window = Window(
+    Const("Hello, unknown person"),
+    Button(Const("Useless button"), id="nothing"),
+    MySG.main,
+)
+dialog = Dialog(main_window)
+registry.register(dialog)
+
+
+@dp.message_handler(commands=["start"])
+async def start(m: Message, dialog_manager: DialogManager):
+    await dialog_manager.start(MySG.main, reset_stack=True)
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
