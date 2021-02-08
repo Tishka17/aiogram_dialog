d4a148adeed511e572bd2f96a8f933193c6ea474
Lamroy95 <50185460+Lamroy95@users.noreply.github.com>
2021-02-08 04:35:50 +0300

    Refactor docs quickstart bot.py

--- docs/examples/quickstart/bot.py
+++ docs/examples/quickstart/bot.py
@@ -4,6 +4,3 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 storage = MemoryStorage()
 bot = Bot(token='BOT TOKEN HERE')
 dp = Dispatcher(bot, storage=storage)
-
-if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
