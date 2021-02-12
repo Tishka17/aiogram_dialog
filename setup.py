06e007f630ad85295ec4b596c79fa6109cd5dc33
Tishka17 <tishka17@mail.ru>
2021-02-12 21:03:41 +0300

    0.9

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ this_directory = path.abspath(path.dirname(__file__))
 setup(
     name='aiogram_dialog',
     description='Mini-framework for dialogs on top of aiogram',
-    version='0.2',
+    version='0.9',
     url='https://github.com/tishka17/aiogram_dialog',
     author='A. Tikhonov',
     author_email='17@itishka.org',
