ad2aad3892aa3619c8a3dc173927ec2c9da33ffd
Tishka17 <tishka17@mail.ru>
2021-04-09 00:19:19 +0300

    jinja environment /closes #7

--- aiogram_dialog/widgets/text/jinja.py
+++ aiogram_dialog/widgets/text/jinja.py
@@ -1,9 +1,16 @@
-from typing import Dict
+from typing import Dict, Any, Iterable, Optional, Callable, Union, Tuple, Mapping
+
+from aiogram import Bot
+from jinja2 import Environment, BaseLoader
 
 from .base import Text
 from ..when import WhenCondition
 from ...manager.manager import DialogManager
-from jinja2 import Template
+
+BOT_ENV_FIELD = "DialogsJinjaEnvironment"
+
+Filter = Callable[..., str]
+Filters = Union[Iterable[Tuple[str, Filter]], Mapping[str, Filter]]
 
 
 class Jinja(Text):
@@ -12,7 +19,33 @@ class Jinja(Text):
         # from file
         # custom filters
         super().__init__(when)
-        self.template = Template(text, trim_blocks=True, lstrip_blocks=True,autoescape=True)
+        self.template_text = text
 
     async def _render_text(self, data: Dict, manager: DialogManager) -> str:
-        return self.template.render(data)
+        env: Environment = manager.event.bot[BOT_ENV_FIELD]
+        template = env.get_template(self.template_text)
+        return template.render(data)
+
+
+class StubLoader(BaseLoader):
+    def get_source(self, environment, template):
+        return template, template, lambda: True
+
+
+def setup_jinja(
+        bot: Bot,
+        *args: Any,
+        filters: Optional[Filters] = None,
+        **kwargs: Any,
+) -> Environment:
+    kwargs.setdefault("autoescape", True)
+    kwargs.setdefault("lstrip_blocks", True)
+    kwargs.setdefault("trim_blocks", True)
+    if not "loader" in kwargs:
+        kwargs["loader"] = StubLoader()
+    env = Environment(*args, **kwargs)
+    if filters is not None:
+        env.filters.update(filters)
+
+    bot[BOT_ENV_FIELD] = env
+    return env
