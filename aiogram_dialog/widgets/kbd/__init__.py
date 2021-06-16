008a42225f71a08e2f62a895709ca50a83a262fb
fregat17 <gavrikovalexey1@gmail.com>
2021-06-16 18:07:35 +0300

    scrolling group and calendar docs, new widgets to kbd __init__

--- aiogram_dialog/widgets/kbd/__init__.py
+++ aiogram_dialog/widgets/kbd/__init__.py
@@ -1,15 +1,19 @@
 from .base import Keyboard
 from .button import Button, Url
+from .calendar_kbd import Calendar
 from .checkbox import Checkbox
 from .group import Group, Row, Column
+from .scrolling_group import ScrollingGroup
 from .select import Select, Radio, Multiselect
 from .state import Back, Cancel, Next, Start, SwitchTo
 
 __all__ = [
     "Keyboard",
     "Button", "Url",
+    "Calendar",
     "Back", "Cancel", "Next", "Start", "SwitchTo",
     "Group", "Row", "Column",
+    "ScrollingGroup",
     "Checkbox",
     "Select", "Radio", "Multiselect",
 ]
