7b31458a2e2427b311f47b34ca58d223ad5c6562
Tishka17 <tishka17@mail.ru>
2021-11-15 15:20:51 +0300

    use cachetools for MediaIdStorage

--- aiogram_dialog/context/media_storage.py
+++ aiogram_dialog/context/media_storage.py
@@ -1,13 +1,14 @@
 from typing import Optional
 
 from aiogram.types import ContentType
+from cachetools import LRUCache
 
 from ..manager.protocols import MediaIdStorageProtocol
 
 
 class MediaIdStorage(MediaIdStorageProtocol):
-    def __init__(self):
-        self.cache = {}
+    def __init__(self, maxsize=10240):
+        self.cache = LRUCache(maxsize=maxsize)
 
     async def get_media_id(
             self, path: Optional[str], type: ContentType,
