5ab56e4946c50701cd3db0e9f6b568d04985fae4
Tishka17 <tishka17@mail.ru>
2021-12-12 19:11:13 +0300

    deprecation warning

--- aiogram_dialog/manager/dialog.py
+++ aiogram_dialog/manager/dialog.py
@@ -2,6 +2,7 @@ from typing import Optional, Any
 
 from aiogram.dispatcher.filters.state import State
 
+from aiogram_dialog.deprecation_utils import manager_deprecated
 from aiogram_dialog.manager.protocols import (
     ManagedDialogProto, DialogManager, ManagedDialogAdapterProto,
 )
@@ -12,19 +13,29 @@ class ManagedDialogAdapter(ManagedDialogAdapterProto):
         self.dialog = dialog
         self.manager = manager
 
-    async def show(self, preview: bool = False):
+    async def show(self, preview: bool = False,
+                   manager: Optional[DialogManager] = None):
+        manager_deprecated(manager)
         return await self.dialog.show(self.manager, preview)
 
-    async def next(self):
+    async def next(self,
+                   manager: Optional[DialogManager] = None):
+        manager_deprecated(manager)
         return await self.dialog.next(self.manager)
 
-    async def back(self):
+    async def back(self,
+                   manager: Optional[DialogManager] = None):
+        manager_deprecated(manager)
         return await self.dialog.back(self.manager)
 
-    async def switch_to(self, state: State):
+    async def switch_to(self, state: State,
+                        manager: Optional[DialogManager] = None):
+        manager_deprecated(manager)
         return await self.dialog.switch_to(state, self.manager)
 
-    def find(self, widget_id) -> Optional[Any]:
+    def find(self, widget_id,
+             manager: Optional[DialogManager] = None) -> Optional[Any]:
+        manager_deprecated(manager)
         widget = self.dialog.find(widget_id)
         if widget is None:
             return None
