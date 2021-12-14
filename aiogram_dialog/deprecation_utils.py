9ee2e03eb4187565ae2aa29975e5d398ce8782c2
Tishka17 <tishka17@mail.ru>
2021-12-14 23:11:31 +0300

    change deprecation version

--- aiogram_dialog/deprecation_utils.py
+++ aiogram_dialog/deprecation_utils.py
@@ -6,7 +6,7 @@ def manager_deprecated(manager: Optional[Any]):
     if manager is not None:
         warnings.warn(
             "Passing `DialogManager` instance directly is deprecated"
-            " and will be removed in aiogram_dialog==1.1",
+            " and will be removed in aiogram_dialog==2.0",
             DeprecationWarning,
             stacklevel=3,
         )
