3c334665f46c1d5704a0e49bacec4940f2405eb0
Tishka17 <tishka17@mail.ru>
2022-01-24 22:52:39 +0300

    on_pre_process_error

--- aiogram_dialog/manager/manager_middleware.py
+++ aiogram_dialog/manager/manager_middleware.py
@@ -25,9 +25,17 @@ class ManagerMiddleware(BaseMiddleware):
     on_pre_process_my_chat_member = on_pre_process_message
 
     async def on_post_process_message(self, _, result, data: dict):
-        manager: DialogManager = data.pop("dialog_manager")
+        manager: DialogManager = data.pop(MANAGER_KEY)
         await manager.close_manager()
 
     on_post_process_callback_query = on_post_process_message
     on_post_process_aiogd_update = on_post_process_message
     on_post_process_my_chat_member = on_post_process_message
+
+    async def on_pre_process_error(self, event: Any, error: Exception,
+                                   data: dict) -> None:
+        await self.on_pre_process_message(event, data)
+
+    async def on_post_process_error(self, event: Any, error: Exception,
+                                    result: list, data: dict) -> None:
+        await self.on_post_process_message(event, result, data)
