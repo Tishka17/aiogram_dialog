70b3124a279dc2b4457accbb45ad4896085e026d
Tishka17 <tishka17@mail.ru>
2022-01-25 00:12:24 +0300

    Fix last media id in storage

--- aiogram_dialog/dialog.py
+++ aiogram_dialog/dialog.py
@@ -122,7 +122,9 @@ class Dialog(ManagedDialogProto):
             )
         message = await manager.show(new_message)
         manager.current_stack().last_message_id = message.message_id
-        manager.current_stack().last_media_id = get_media_id(message)
+        media_id = get_media_id(message)
+        manager.current_stack().last_media_id = media_id.file_id
+        manager.current_stack().last_media_unique_id = media_id.file_unique_id
         if new_message.media:
             await media_id_storage.save_media_id(
                 path=new_message.media.path,
