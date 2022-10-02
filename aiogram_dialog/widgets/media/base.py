2df457c14f36357c0da96847ec43e92ef6d00f82
Tishka17 <tishka17@mail.ru>
2022-10-02 22:25:54 +0400

    fix media subclassing

--- aiogram_dialog/widgets/media/base.py
+++ aiogram_dialog/widgets/media/base.py
@@ -15,7 +15,7 @@ class Media(BaseWidget, Whenable, MediaWidget):
     ) -> Optional[MediaAttachment]:
         if not self.is_(data, manager):
             return None
-        return await self._render_media(data, DialogManager)
+        return await self._render_media(data, manager)
 
     async def _render_media(
             self, data: Any, manager: DialogManager,
