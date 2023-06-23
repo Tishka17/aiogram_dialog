5bb6ca2453b8409dd0900cbe23d7ea5a0c552c68
Tishka17 <tishka17@mail.ru>
2023-06-22 23:14:17 -0300

    error handler for mega example

--- example/mega/bot.py
+++ example/mega/bot.py
@@ -3,10 +3,13 @@ import logging
 import os.path
 
 from aiogram import Bot, Dispatcher, F, Router
+from aiogram.exceptions import TelegramBadRequest
+from aiogram.filters import ExceptionTypeFilter
 from aiogram.fsm.storage.memory import MemoryStorage
-from aiogram.types import Message
+from aiogram.types import Message, ErrorEvent
 
-from aiogram_dialog import DialogManager, setup_dialogs, StartMode
+from aiogram_dialog import DialogManager, setup_dialogs, StartMode, ShowMode
+from aiogram_dialog.api.exceptions import UnknownIntent
 from bot_dialogs import states
 from bot_dialogs.calendar import calendar_dialog
 from bot_dialogs.counter import counter_dialog
@@ -21,6 +24,25 @@ from bot_dialogs.switch import switch_dialog
 async def start(message: Message, dialog_manager: DialogManager):
     # it is important to reset stack because user wants to restart everything
     await dialog_manager.start(states.Main.MAIN, mode=StartMode.RESET_STACK)
+
+
+async def on_unknown_intent(event: ErrorEvent, dialog_manager: DialogManager):
+    """Example of handling UnknownIntent Error and starting new dialog."""
+    logging.error("Restarting dialog: %s", event.exception)
+    if event.update.callback_query:
+        await event.update.callback_query.answer(
+            "Bot process was restarted due to maintenance.\n"
+            "Redirecting to main menu.",
+        )
+        try:
+            await event.update.callback_query.message.delete()
+        except TelegramBadRequest:
+            pass  # whatever
+    await dialog_manager.start(
+        states.Main.MAIN,
+        mode=StartMode.RESET_STACK,
+        show_mode=ShowMode.SEND,
+    )
 
 
 dialog_router = Router()
@@ -44,6 +66,10 @@ async def main():
     storage = MemoryStorage()
     dp = Dispatcher(storage=storage)
     dp.message.register(start, F.text == "/start")
+    dp.errors.register(
+        on_unknown_intent,
+        ExceptionTypeFilter(UnknownIntent),
+    )
     dp.include_router(dialog_router)
     setup_dialogs(dp)
 
