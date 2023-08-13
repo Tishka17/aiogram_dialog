b48da05c23eb36c52a58c67b0dcb5ceb8632fc93
Tishka17 <tishka17@mail.ru>
2023-08-14 01:03:11 +0400

    /closes #306 python 3.9.7 inheritance

--- src/aiogram_dialog/widgets/media/base.py
+++ src/aiogram_dialog/widgets/media/base.py
@@ -6,7 +6,7 @@ from aiogram_dialog.api.protocols import DialogManager
 from aiogram_dialog.widgets.common import BaseWidget, Whenable, WhenCondition
 
 
-class Media(BaseWidget, Whenable, MediaWidget):
+class Media(Whenable, BaseWidget, MediaWidget):
     def __init__(self, when: WhenCondition = None):
         super().__init__(when=when)
 
