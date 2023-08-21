f6ce53a0de2bf77b5bcf0b6f0657ccf50cdce09c
Tishka17 <tishka17@mail.ru>
2023-08-21 21:54:08 +0400

    ManagedTextInput

--- src/aiogram_dialog/widgets/input/__init__.py
+++ src/aiogram_dialog/widgets/input/__init__.py
@@ -4,8 +4,9 @@ __all__ = [
     "BaseInput",
     "CombinedInput",
     "TextInput",
+    "ManagedTextInput",
 ]
 
 from .base import BaseInput, MessageHandlerFunc, MessageInput
 from .combined import CombinedInput
-from .text import TextInput
+from .text import TextInput, ManagedTextInput
