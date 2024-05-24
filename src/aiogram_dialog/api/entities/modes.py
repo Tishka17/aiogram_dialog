e0570e93003a35437652c2e5a1e71071c9c2c160
chiri <109767616+yachirig@users.noreply.github.com>
2024-05-24 08:47:57 +0300

    Update formatting for ShowMode docs

--- src/aiogram_dialog/api/entities/modes.py
+++ src/aiogram_dialog/api/entities/modes.py
@@ -5,25 +5,25 @@ class ShowMode(Enum):
     """
     Modes of show dialog message when new update handled.
 
-    AUTO:
+    **AUTO**:
         default show mode.
 
         Uses `SEND mode` when new message from user handled or `EDIT mode`
         when any other updated handled.
 
-    EDIT:
+    **EDIT**:
         edit dialog message
 
-    SEND:
+    **SEND****:
         send new dialog message
 
-    DELETE_AND_SEND:
+    **DELETE_AND_SEND**:
         delete and send new dialog message
 
         `Attention`: Telegram's restrictions will prevent the deletion of the
         message when more than 2 days has elapsed.
 
-    NO_UPDATE:
+    **NO_UPDATE**:
         will not update and rerender the dialog message
     """
 
